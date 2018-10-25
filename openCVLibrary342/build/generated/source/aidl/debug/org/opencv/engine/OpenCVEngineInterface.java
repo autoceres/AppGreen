c80b3ab98920ecddd7321c577c4007a24f5302d2
Felipe Weber <weberecomp@gmail.com>
2018-10-25 16:27:05 -0300

    Habilitado o suporte bluetooth.

--- openCVLibrary342/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary342/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: F:\\AndroidStudioProjects\\AppGreen\\openCVLibrary342\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: /Users/felipe/StudioProjects/AppGreen/openCVLibrary342/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
